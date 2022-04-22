64650ffd23864bca09e82ef5f0e501d42260fb51
nullparrot <github@weist.dev>
2022-04-21 19:59:56 -0700

    css for dates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-console.log("This does nothing")
+currentYear = new Date().getFullYear();
+document.querySelector('#year').innerHTML = currentYear
+lastUpdated = document.lastModified;
+document.getElementById("last-updated").innerHTML = lastUpdated
