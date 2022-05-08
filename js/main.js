f6bf728c07c89dd151d97b609dc28be7f9bec5fc
Andrew Weist <github@weist.dev>
2022-05-07 22:01:06 -0700

    Site plan dev

--- js/main.js
+++
@@ -1,4 +0,0 @@
-currentYear = new Date().getFullYear();
-document.querySelector('#year').innerHTML = currentYear
-lastUpdated = document.lastModified;
-document.getElementById("last-updated").innerHTML = lastUpdated
