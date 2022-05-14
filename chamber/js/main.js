f8f77d87e748e77c82313814a0417e5f0ea011e9
Andrew Weist <github@weist.dev>
2022-05-13 17:04:44 -0700

    camber ioof commerce starting

---
+++ chamber/js/main.js
@@ -0,0 +1,4 @@
+currentYear = new Date().getFullYear();
+document.querySelector('#year').innerHTML = currentYear
+lastUpdated = document.lastModified;
+document.getElementById("last-updated").innerHTML = lastUpdated
