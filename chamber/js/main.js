46d06f143a4d2dcb18efc9e91be701e865bdae82
Andrew Weist <github@weist.dev>
2022-05-14 17:18:56 -0700

    Commerce site, css for small and medium

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,4 +1,12 @@
-currentYear = new Date().getFullYear();
-document.querySelector('#year').innerHTML = currentYear
+currentDate = new Date();
+document.getElementById("currentDate").innerHTML =
+  currentDate.toLocaleDateString("en-UK", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+currentYear = currentDate.getFullYear();
+document.querySelector("#year").innerHTML = currentYear;
 lastUpdated = document.lastModified;
-document.getElementById("last-updated").innerHTML = lastUpdated
+document.getElementById("last-updated").innerHTML = lastUpdated;
